c07c272e4ce01cce355cc6f8b5fd686a1735ef8b
mdjabbarmollah <mdjabbarmollah718@gmail.com>
2026-08-28 11:34:49 +0600

    Add files via upload

---
+++ HTML/Day -9/naming.js
@@ -0,0 +1,39 @@
+let location = "bangla"
+let age = 18;
+let boyos = 18;
+// 1..// meaning full word use kora jabe na naming er khetre
+let dsfsdklg = 87;
+let ave = 90;
+let info = "mon majar"
+let brb = 'jasiya'
+
+// 2..// jasob word keyword se gulo use kora jabe na like the word is let (it's aslo cald reserved word)
+
+// const let = "fahad"
+// const false = 'false';
+
+// 4..// has to be in one number 
+
+const her_phone_number = 13932432432;//it called sanke case
+
+// camel case (camel case amra besi use korbo?most of the time we should be use camel case)
+// first later lower case & second word first later upercase like firstName, totalPrice,
+// start with a letter $ or _ 
+
+  const her_Phone_Number = 13932432432;
+
+// pascal case  
+
+const HerPhoneNumber = 34239975;
+
+// name cannot start with a Number.but could be used anaywhere other than the the first latter
+// const 3person = 5; don't allow like this line
+const person4 = 7; 
+// it should be like this 
+// 5..other than latter name can start with either
+
+// 6... case sensitive
+
+let address = 'katabon'
+let Address = "sundarbar this line define another variable for first uparcase later"
+let AddRess = 'modhubon(this is another one variable because their are also have different case later)'
---
+++ HTML/Day -9/substract.js
@@ -0,0 +1,5 @@
+// console.log(50 + 24);
+// console.log(50 - 25);
+const shirtprice = 500;
+const pantprice = 300;
+console.log(pantprice - shirtprice);
